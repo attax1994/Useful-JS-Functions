a0b135a291e19377f3c64cd857a508d20e601fb4
attax1994 <lqytyui@qq.com>
2018-02-08 14:52:30 +0800

    currify

--- common/Currify.ts
+++ common/Currify.ts
@@ -2,6 +2,6 @@ function currify(fn: Function) {
     const args = [].slice.call(arguments, 1);
     return function () {
         const _inargs = Array.from(arguments);
-        return fn.apply(null, args.concat(_inargs));
+        return fn.apply(null, [...args, ..._inargs]);
     };
 }
