25c8b090ee0eeec058bae3a0733ead008077672e
attax1994 <lqytyui@qq.com>
2018-03-14 15:07:08 +0800

    promise

---
+++ common/Promise.js
@@ -0,0 +1,170 @@
+// 状态机
+var PENDING = 0;
+var FULFILLED = 1;
+var REJECTED = 2;
+
+function Promise(fn) {
+    // 都是private属性
+    var state = PENDING;
+    var value = null;
+    var handlers = [];
+
+
+    /**
+     * 将状态改为成功，要改变状态，放进结果值，并逐个执行回调
+     * @param {*} result 
+     */
+    function fulfill(result) {
+        state = FULFILLED;
+        value = result;
+        // 逐个传给handle()函数，执行回调
+        handlers.forEach(handle);
+        // 最后handlers置空
+        handlers = null;
+    }
+
+
+    /**
+     * 将状态改为失败，要改变状态，放进结果值，并逐个执行回调
+     * @param {*} error 
+     */
+    function reject(error) {
+        state = REJECTED;
+        value = error;
+        // 逐个传给handle()函数，执行回调
+        handlers.forEach(handle);
+        // 最后handlers置空
+        handlers = null;
+    }
+
+
+    /**
+     * Promise的resolve方法，
+     * @param {*} result 
+     */
+    function resolve(result) {
+        try {
+            var then = getThen(result);
+            if (then) {
+                doResolve(then.bind(result), resolve, reject);
+                return;
+            }
+        } catch (e) {
+            reject(e);
+        }
+    }
+
+
+    /**
+     * 执行回调。
+     * 根据state去判断其行为，PENDING压入handlers栈，FULFILLED执行onFulfilled，REJECTED执行onRejected
+     * @param {*} handler 
+     */
+    function handle(handler) {
+        // 异步还没有完成，handler入栈
+        if (state === PENDING) {
+            handlers.push(handler);
+        } else {
+            if (state === FULFILLED && typeof handler.onFulfilled === 'function') {
+                handler.onFulfilled(value);
+            }
+            if (state === REJECTED && typeof handler.onRejected === 'function') {
+                handler.onRejected(value);
+            }
+        }
+    }
+
+
+    /**
+    * 返回目标的then方法
+    * @param {*} value 
+    */
+    function getThen(value) {
+        var t = typeof value;
+        if (value && (t === 'ojbect' || t === 'function')) {
+            var then = value.then;
+            if (typeof then === 'function') {
+                return then;
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * 
+     * @param {*} fn 
+     * @param {*} onFulfilled 
+     * @param {*} onRejected 
+     */
+    function doResolve(fn, onFulfilled, onRejected) {
+        var done = false;
+        try {
+            fn(function (value) {
+                if (done) return;
+                done = true;
+                onFulfilled(value);
+            }, function (reason) {
+                if (done) return;
+                done = true;
+                onRejected(value);
+            });
+        } catch (ex) {
+            if (done) return;
+            done = true;
+            onRejected(ex);
+        }
+    }
+
+
+    /**
+     * then方法，负责注册或执行下一步的回调，返回重新包装过的Promise对象(thenable)
+     * @param {*} onFulfilled 
+     * @param {*} onRejected 
+     */
+    this.then = function (onFulfilled, onRejected) {
+        var self = this;
+        return new Promise(function (resolve, reject) {
+            return self.done(function (result) {
+                if (typeof onFulfilled === 'function') {
+                    try {
+                        return resolve(onFulfilled(result));
+                    } catch (ex) {
+                        return reject(ex);
+                    }
+                } else {
+                    return resolve(result);
+                }
+            }, function (error) {
+                if (typeof onRejected === 'function') {
+                    try {
+                        return resolve(onRejected(error));
+                    } catch (ex) {
+                        return reject(ex);
+                    }
+                } else {
+                    return reject(error);
+                }
+            });
+        });
+    };
+
+
+    /**
+     * 类似于finally方法，结束后不会再返回thenable的对象。
+     * @param {*} onFulfilled 
+     * @param {*} onRejected 
+     */
+    this.done = function (onFulfilled, onRejected) {
+        // 异步执行
+        setTimeout(function () {
+            handle({
+                onFulfilled: onFulfilled,
+                onRejected: onRejected,
+            });
+        }, 0);
+    };
+
+    doResolve(fn, resolve, reject);
+}
+
