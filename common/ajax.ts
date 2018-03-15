4b4e5b7c4f9b5e964e3dcb1f325bd7efcdbc235b
attax1994 <lqytyui@qq.com>
2018-03-15 15:58:08 +0800

    json stringify for ajax

--- common/ajax.ts
+++ common/ajax.ts
@@ -29,7 +29,48 @@
             xhr.open(method, url, true);
 
             if (method === 'POST' || method === 'PUT') {
-                xhr.send(body);
+                xhr.send(JSON.stringify(body));
+            } else {
+                xhr.send(null);
+            }
+
+        });
+    }
+
+    window['ajax'] = ajax;
+
+})();
+
+
+/**
+ * 开箱即用的JS版本
+ */
+; (function () {
+
+    const onreadystatechangeHandler = function (xhr, resolve, reject) {
+        if (xhr.readyState === 4) {
+            const status = xhr.status.toString().substring(0, 1);
+            if (status === '2' || status === '3') {
+                resolve(xhr.response);
+            } else {
+                reject(xhr.response);
+            }
+        }
+    };
+
+
+    const ajax = function (requestParams) {
+        const { url, method, header = {}, body = {} } = requestParams;
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = onreadystatechangeHandler.bind(this, xhr, resolve, reject);
+            for (var key in header) {
+                xhr.setRequestHeader(key, header[key]);
+            }
+            xhr.open(method, url, true);
+
+            if (method === 'POST' || method === 'PUT') {
+                xhr.send(JSON.stringify(body));
             } else {
                 xhr.send(null);
             }
