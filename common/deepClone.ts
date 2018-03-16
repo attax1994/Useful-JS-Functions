892f78151fd61a7e86734ba7690b14779106fa06
attax1994 <lqytyui@qq.com>
2018-03-16 12:46:26 +0800

    clone optimization

--- common/deepClone.ts
+++ common/deepClone.ts
@@ -1,25 +1,30 @@
 /**
-   * Clone the value of an Object based Array or Object both with or without prototype.
-   * Returns the new target which does not share the same pointer as source.
-   * @param source : any
-   * @returns any
-   */
+* Clone the value of an Object based Array or Object both with or without prototype.
+* Returns the new target which does not share the same pointer as source.
+* @param source : any
+* @returns any
+*/
 function deepClone(source: any) {
+    if (!(source instanceof Object)) {
+        return source;
+    }
+
     if (Object.prototype.toString.call(source) === '[object Array]') {
         return source.map((value) => {
-            if (Object.prototype.toString.call(value) === '[object Array]') {
-                return deepClone(value);
-            } else if (Object.prototype.toString.call(value) === '[object Object]') {
-                return Object.assign(Object.create(Object.getPrototypeOf(value)), value);
-            } else {
-                return JSON.parse(JSON.stringify(value));
-            }
+            return deepClone(value);
         });
     }
     if (Object.prototype.toString.call(source) === '[object Object]') {
-        return Object.assign(Object.create(Object.getPrototypeOf(source)), source);
+        let cloned = {};
+        // 原型链继承
+        Object.setPrototypeOf(cloned, Object.getPrototypeOf(source));
+        // 属性处理
+        for (let key in source) {
+            cloned[key] = deepClone(source[key]);
+        }
+
+        return cloned;
     }
-    return JSON.parse(JSON.stringify(source));
 }
 
 
@@ -27,29 +32,29 @@ function deepClone(source: any) {
 function clone(objectToBeCloned) {
     // Basis.
     if (!(objectToBeCloned instanceof Object)) {
-      return objectToBeCloned;
+        return objectToBeCloned;
     }
-  
+
     var objectClone;
-    
+
     // Filter out special objects.
     var Constructor = objectToBeCloned.constructor;
     switch (Constructor) {
-      // Implement other special objects here.
-      case RegExp:
-        objectClone = new Constructor(objectToBeCloned);
-        break;
-      case Date:
-        objectClone = new Constructor(objectToBeCloned.getTime());
-        break;
-      default:
-        objectClone = new Constructor();
+        // Implement other special objects here.
+        case RegExp:
+            objectClone = new Constructor(objectToBeCloned);
+            break;
+        case Date:
+            objectClone = new Constructor(objectToBeCloned.getTime());
+            break;
+        default:
+            objectClone = new Constructor();
     }
-    
+
     // Clone each property.
     for (var prop in objectToBeCloned) {
-      objectClone[prop] = clone(objectToBeCloned[prop]);
+        objectClone[prop] = clone(objectToBeCloned[prop]);
     }
-    
+
     return objectClone;
-  }
+}
