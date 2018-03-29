3a7dd118dfd821b6d1784918a74492a730a714b3
attax1994 <lqytyui@qq.com>
2018-03-29 21:59:41 +0800

    module manager

--- common/AMD&CMD.js
+++ common/AMD&CMD.js
@@ -22,3 +22,59 @@ define('myModule', function (require, exports, module) {
     'use strict';
     let moduleOne = require('moduleOne');
 });
+
+
+var ModuleManager = (function () {
+    var _modules = {};
+
+    function getModule(name) {
+        var _module = _modules[name];
+        return _module || {};
+    }
+
+    function define(name, deps, fn) {
+        if (typeof name !== 'string') {
+            throw new TypeError('name should be string.');
+        }
+        if (_modules[name]) {
+            throw new Error('This module has been already declared.');
+        }
+        if (!deps instanceof Array) {
+            throw new TypeError('deps should be Array<String>.');
+        }
+        if (!fn instanceof Function) {
+            throw new TypeError('fn should be function.');
+        }
+
+        var depsModules = [];
+        var len = deps.length;
+        for (var i = 0; i < len; i++) {
+            depsModules.push(getModule(deps[i]));
+        }
+
+        _modules[name] = fn.apply(null, depsModules);
+        return true;
+    }
+
+    function require(deps, fn) {
+        if (!deps instanceof Array) {
+            throw new TypeError('deps should be Array<String>.');
+        }
+        if (!fn instanceof Function) {
+            throw new TypeError('fn should be function.');
+        }
+
+        var depsModules = [];
+        var len = deps.length;
+        for (var i = 0; i < len; i++) {
+            depsModules.push(getModule(deps[i]));
+        }
+
+        fn.apply(null, depsModules);
+    }
+
+    return {
+        define: define,
+        require: require,
+    };
+})();
