72e9c8a9d9ac03ad4df39e1ee35d66b9a09ef73c
attax1994 <lqytyui@qq.com>
2018-03-31 13:53:18 +0800

    currency

--- common/currency.ts
+++ common/currency.ts
@@ -3,3 +3,12 @@
  */
 let x = 0 / 100;
 x.toLocaleString("en-us", { style: "currency", currency: 'CNY', minimumFractionDigits: 2 });
+
+var arr = 132456485.4.toFixed(2).split('').reverse();
+var result = arr
+  .map((value, index) => {
+    return ((index + 1) % 3 || index + 1 === arr.length || index + 1 === 3) ? value : ',' + value;
+  })
+  .reverse()
+  .join('');
+console.log(result)
