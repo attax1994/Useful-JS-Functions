7da0bb0a0806c04f006e2688bf1aae17a72f45ea
attax1994 <lqytyui@qq.com>
2019-05-05 14:11:29 +0800

    feat(algorithm/sort/selection): selection srot

---
+++ algorithm/sort/selection.ts
@@ -0,0 +1,21 @@
+function selectionSort(arr: number[]) {
+  for (let i = 0, len = arr.length; i < len; i++) {
+    let min = i
+    // 每一趟从后面的无序区选取最小的数挪到当前索引下
+    for (let j = i + 1; j < len; j++) {
+      if (less(arr[j], arr[min])) { min = j }
+    }
+    swap(arr, i, min)
+  }
+  return arr
+}
+
+function less(left: number, right: number): boolean {
+  return left < right
+}
+
+function swap(arr: number[], leftIndex: number, rightIndex: number) {
+  let temp = arr[leftIndex]
+  arr[leftIndex] = arr[rightIndex]
+  arr[rightIndex] = temp
+}
