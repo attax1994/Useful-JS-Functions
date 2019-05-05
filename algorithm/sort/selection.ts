6e28cd94df8d8de60a2020eb4e0f7ab2a297df0f
attax1994 <lqytyui@qq.com>
2019-05-05 14:32:51 +0800

    chore(algorithm/sort/selection): optimize code style

--- algorithm/sort/selection.ts
+++ algorithm/sort/selection.ts
@@ -1,21 +1,25 @@
-function selectionSort(arr: number[]) {
-  for (let i = 0, len = arr.length; i < len; i++) {
-    let min = i
-    // 每一趟从后面的无序区选取最小的数挪到当前索引下
-    for (let j = i + 1; j < len; j++) {
-      if (less(arr[j], arr[min])) { min = j }
+; (function (global: Window) {
+  function selectionSort(arr: number[]) {
+    for (let i = 0, len = arr.length; i < len; i++) {
+      let min = i
+      // 每一趟从后面的无序区选取最小的数挪到当前索引下
+      for (let j = i + 1; j < len; j++) {
+        if (less(arr[j], arr[min])) { min = j }
+      }
+      swap(arr, i, min)
     }
-    swap(arr, i, min)
+    return arr
   }
-  return arr
-}
 
-function less(left: number, right: number): boolean {
-  return left < right
-}
+  function less(left: number, right: number): boolean {
+    return left < right
+  }
 
-function swap(arr: number[], leftIndex: number, rightIndex: number) {
-  let temp = arr[leftIndex]
-  arr[leftIndex] = arr[rightIndex]
-  arr[rightIndex] = temp
-}
+  function swap(arr: number[], leftIndex: number, rightIndex: number) {
+    let temp: number = arr[leftIndex]
+    arr[leftIndex] = arr[rightIndex]
+    arr[rightIndex] = temp
+  }
+
+  (global as any).selectionSort = selectionSort
+}(window));
