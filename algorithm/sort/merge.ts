a9fb60bb4aedea4ec4c35d7fd4ed84ba1f57b458
attax1994 <lqytyui@qq.com>
2019-05-05 13:59:18 +0800

    chore(algorithm/sort/merge): optimize code style

--- algorithm/sort/merge.ts
+++ algorithm/sort/merge.ts
@@ -1,32 +1,29 @@
 // 采用自上而下的递归方法
 function mergeSort(arr: Array<number>) {
-    var len = arr.length;
-    if (len < 2) {
-        return arr;
-    }
-    var middle = Math.floor(len / 2),
-        left = arr.slice(0, middle),
-        right = arr.slice(middle);
-    return merge(mergeSort(left), mergeSort(right));
+    let len = arr.length
+    if (len < 2) { return arr }
+
+    let middle = Math.floor(len / 2)
+        , left = arr.slice(0, middle)
+        , right = arr.slice(middle)
+    return merge(mergeSort(left), mergeSort(right))
 }
 
 function merge(left: Array<number>, right: Array<number>) {
-    var result = [];
+    var result = []
 
     while (left.length && right.length) {
         if (left[0] <= right[0]) {
-            result.push(left.shift());
+            result.push(left.shift())
         } else {
-            result.push(right.shift());
+            result.push(right.shift())
         }
     }
 
     // 把未写入的大数继续写入
-    while (left.length)
-        result.push(left.shift());
+    while (left.length) { result.push(left.shift()) }
 
-    while (right.length)
-        result.push(right.shift());
+    while (right.length) { result.push(right.shift()) }
 
-    return result;
+    return result
 }
